44d3455a21aadd00ed99c69eddf8fa68f21729c6
Aditya shishu <snake@snake.com>
2018-08-18 10:18:26 +0000

    commit 5: final commit

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,5 @@
-
-
-//this is very import codew
 public class Main {
     public static void main(String[] args) {
- 
-       System.out.println("Hello, World!"
-/SYSTEMC.COUT 
+        System.out.println("Hello, World!");
     }
 }
